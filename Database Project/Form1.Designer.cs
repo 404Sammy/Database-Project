c2c34834fd8ff577e3923df4682fefba36fc04bb
404Sammy <samuelkipkorir55@gmail.com>
2024-07-11 01:21:05 +0300

    Add project files.

---
+++ Database Project/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Database_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TxtGenre = new System.Windows.Forms.TextBox();
+            this.TxtYear = new System.Windows.Forms.TextBox();
+            this.TxtAuthor = new System.Windows.Forms.TextBox();
+            this.TxtTitle = new System.Windows.Forms.TextBox();
+            this.dgvBooks = new System.Windows.Forms.DataGridView();
+            this.booksBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.libraryDBDataSet1 = new Database_Project.LibraryDBDataSet1();
+            this.booksBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.libraryDBDataSet = new Database_Project.LibraryDBDataSet();
+            this.LblTitle = new System.Windows.Forms.Label();
+            this.LblAuthor = new System.Windows.Forms.Label();
+            this.LblYear = new System.Windows.Forms.Label();
+            this.LblGenre = new System.Windows.Forms.Label();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.booksTableAdapter = new Database_Project.LibraryDBDataSetTableAdapters.BooksTableAdapter();
+            this.booksTableAdapter1 = new Database_Project.LibraryDBDataSet1TableAdapters.BooksTableAdapter();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tITLEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.aUTHORDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yearPublishedDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gENREDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libraryDBDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libraryDBDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtGenre
+            // 
+            this.TxtGenre.Location = new System.Drawing.Point(51, 228);
+            this.TxtGenre.Name = "TxtGenre";
+            this.TxtGenre.Size = new System.Drawing.Size(100, 20);
+            this.TxtGenre.TabIndex = 0;
+            // 
+            // TxtYear
+            // 
+            this.TxtYear.Location = new System.Drawing.Point(51, 159);
+            this.TxtYear.Name = "TxtYear";
+            this.TxtYear.Size = new System.Drawing.Size(100, 20);
+            this.TxtYear.TabIndex = 1;
+            // 
+            // TxtAuthor
+            // 
+            this.TxtAuthor.Location = new System.Drawing.Point(51, 94);
+            this.TxtAuthor.Name = "TxtAuthor";
+            this.TxtAuthor.Size = new System.Drawing.Size(100, 20);
+            this.TxtAuthor.TabIndex = 2;
+            // 
+            // TxtTitle
+            // 
+            this.TxtTitle.Location = new System.Drawing.Point(51, 42);
+            this.TxtTitle.Name = "TxtTitle";
+            this.TxtTitle.Size = new System.Drawing.Size(100, 20);
+            this.TxtTitle.TabIndex = 3;
+            // 
+            // dgvBooks
+            // 
+            this.dgvBooks.AutoGenerateColumns = false;
+            this.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBooks.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.tITLEDataGridViewTextBoxColumn,
+            this.aUTHORDataGridViewTextBoxColumn,
+            this.yearPublishedDataGridViewTextBoxColumn,
+            this.gENREDataGridViewTextBoxColumn});
+            this.dgvBooks.DataSource = this.booksBindingSource1;
+            this.dgvBooks.Location = new System.Drawing.Point(303, 9);
+            this.dgvBooks.Name = "dgvBooks";
+            this.dgvBooks.Size = new System.Drawing.Size(527, 150);
+            this.dgvBooks.TabIndex = 4;
+            this.dgvBooks.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBooks_CellContentClick);
+            // 
+            // booksBindingSource1
+            // 
+            this.booksBindingSource1.DataMember = "Books";
+            this.booksBindingSource1.DataSource = this.libraryDBDataSet1;
+            // 
+            // libraryDBDataSet1
+            // 
+            this.libraryDBDataSet1.DataSetName = "LibraryDBDataSet1";
+            this.libraryDBDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // booksBindingSource
+            // 
+            this.booksBindingSource.DataMember = "Books";
+            this.booksBindingSource.DataSource = this.libraryDBDataSet;
+            // 
+            // libraryDBDataSet
+            // 
+            this.libraryDBDataSet.DataSetName = "LibraryDBDataSet";
+            this.libraryDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // LblTitle
+            // 
+            this.LblTitle.AutoSize = true;
+            this.LblTitle.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTitle.Location = new System.Drawing.Point(48, 9);
+            this.LblTitle.Name = "LblTitle";
+            this.LblTitle.Size = new System.Drawing.Size(43, 19);
+            this.LblTitle.TabIndex = 5;
+            this.LblTitle.Text = "TITLE";
+            // 
+            // LblAuthor
+            // 
+            this.LblAuthor.AutoSize = true;
+            this.LblAuthor.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblAuthor.Location = new System.Drawing.Point(48, 73);
+            this.LblAuthor.Name = "LblAuthor";
+            this.LblAuthor.Size = new System.Drawing.Size(68, 19);
+            this.LblAuthor.TabIndex = 6;
+            this.LblAuthor.Text = "AUTHOR";
+            // 
+            // LblYear
+            // 
+            this.LblYear.AutoSize = true;
+            this.LblYear.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblYear.Location = new System.Drawing.Point(48, 129);
+            this.LblYear.Name = "LblYear";
+            this.LblYear.Size = new System.Drawing.Size(108, 19);
+            this.LblYear.TabIndex = 7;
+            this.LblYear.Text = "Year Published";
+            // 
+            // LblGenre
+            // 
+            this.LblGenre.AutoSize = true;
+            this.LblGenre.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblGenre.Location = new System.Drawing.Point(48, 194);
+            this.LblGenre.Name = "LblGenre";
+            this.LblGenre.Size = new System.Drawing.Size(53, 19);
+            this.LblGenre.TabIndex = 8;
+            this.LblGenre.Text = "GENRE";
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAdd.Location = new System.Drawing.Point(328, 177);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(75, 23);
+            this.BtnAdd.TabIndex = 9;
+            this.BtnAdd.Text = "ADD";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnUpdate.Location = new System.Drawing.Point(542, 178);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(92, 23);
+            this.BtnUpdate.TabIndex = 10;
+            this.BtnUpdate.Text = "UPDATE";
+            this.BtnUpdate.UseVisualStyleBackColor = true;
+            this.BtnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDelete.Location = new System.Drawing.Point(670, 178);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.BtnDelete.Size = new System.Drawing.Size(75, 23);
+            this.BtnDelete.TabIndex = 11;
+            this.BtnDelete.Text = "DELETE";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnClear.Location = new System.Drawing.Point(441, 177);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(75, 23);
+            this.BtnClear.TabIndex = 12;
+            this.BtnClear.Text = "CLEAR";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // booksTableAdapter
+            // 
+            this.booksTableAdapter.ClearBeforeFill = true;
+            // 
+            // booksTableAdapter1
+            // 
+            this.booksTableAdapter1.ClearBeforeFill = true;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // tITLEDataGridViewTextBoxColumn
+            // 
+            this.tITLEDataGridViewTextBoxColumn.DataPropertyName = "TITLE";
+            this.tITLEDataGridViewTextBoxColumn.HeaderText = "TITLE";
+            this.tITLEDataGridViewTextBoxColumn.Name = "tITLEDataGridViewTextBoxColumn";
+            // 
+            // aUTHORDataGridViewTextBoxColumn
+            // 
+            this.aUTHORDataGridViewTextBoxColumn.DataPropertyName = "AUTHOR";
+            this.aUTHORDataGridViewTextBoxColumn.HeaderText = "AUTHOR";
+            this.aUTHORDataGridViewTextBoxColumn.Name = "aUTHORDataGridViewTextBoxColumn";
+            // 
+            // yearPublishedDataGridViewTextBoxColumn
+            // 
+            this.yearPublishedDataGridViewTextBoxColumn.DataPropertyName = "YearPublished";
+            this.yearPublishedDataGridViewTextBoxColumn.HeaderText = "YearPublished";
+            this.yearPublishedDataGridViewTextBoxColumn.Name = "yearPublishedDataGridViewTextBoxColumn";
+            // 
+            // gENREDataGridViewTextBoxColumn
+            // 
+            this.gENREDataGridViewTextBoxColumn.DataPropertyName = "GENRE";
+            this.gENREDataGridViewTextBoxColumn.HeaderText = "GENRE";
+            this.gENREDataGridViewTextBoxColumn.Name = "gENREDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 450);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.LblGenre);
+            this.Controls.Add(this.LblYear);
+            this.Controls.Add(this.LblAuthor);
+            this.Controls.Add(this.LblTitle);
+            this.Controls.Add(this.dgvBooks);
+            this.Controls.Add(this.TxtTitle);
+            this.Controls.Add(this.TxtAuthor);
+            this.Controls.Add(this.TxtYear);
+            this.Controls.Add(this.TxtGenre);
+            this.Name = "Form1";
+            this.Text = "Library Management System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libraryDBDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libraryDBDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtGenre;
+        private System.Windows.Forms.TextBox TxtYear;
+        private System.Windows.Forms.TextBox TxtAuthor;
+        private System.Windows.Forms.TextBox TxtTitle;
+        private System.Windows.Forms.DataGridView dgvBooks;
+        private System.Windows.Forms.Label LblTitle;
+        private System.Windows.Forms.Label LblAuthor;
+        private System.Windows.Forms.Label LblYear;
+        private System.Windows.Forms.Label LblGenre;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Button BtnClear;
+        private LibraryDBDataSet libraryDBDataSet;
+        private System.Windows.Forms.BindingSource booksBindingSource;
+        private LibraryDBDataSetTableAdapters.BooksTableAdapter booksTableAdapter;
+        private LibraryDBDataSet1 libraryDBDataSet1;
+        private System.Windows.Forms.BindingSource booksBindingSource1;
+        private LibraryDBDataSet1TableAdapters.BooksTableAdapter booksTableAdapter1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tITLEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aUTHORDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yearPublishedDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gENREDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Database Project/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Input;
+using System.Diagnostics;
+using System.Data.Common;
+using System.Net.NetworkInformation;
+
+
+namespace Database_Project
+{
+
+    public partial class Form1 : Form
+
+    {
+        string connectionString = @"Server=PCR\MSSQLSERVER1;Database=LibraryDB;Integrated Security=True;";
+        int ID;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void RefreshDataGrid()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Books", con);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dgvBooks.DataSource = dt;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'libraryDBDataSet1.Books' table. You can move, or remove it, as needed.
+            this.booksTableAdapter1.Fill(this.libraryDBDataSet1.Books);
+
+
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            // Get values from textboxes
+            string title = TxtTitle.Text; // Assuming there's a TextBox named txtTitle
+            string author = TxtAuthor.Text; // Assuming there's a TextBox named txtAuthor
+            string genre = TxtGenre.Text;
+            int YearPublished;
+            if (!int.TryParse(TxtYear.Text, out YearPublished))
+            {
+                MessageBox.Show("Please enter a valid year.");
+                return;
+            }
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "INSERT INTO Books (Title, Author,Genre,YearPublished) VALUES (@Title, @Author,@Genre,@YearPublished)";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Title", title);
+                    cmd.Parameters.AddWithValue("@Author", author);
+                    cmd.Parameters.AddWithValue("@Genre", genre);
+                    cmd.Parameters.AddWithValue("@YearPublished", YearPublished);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            // Ensure a row is selected in the DataGridView
+            if (dgvBooks.SelectedRows.Count > 0)
+            {
+                // Get the ID from the selected row
+                int id = Convert.ToInt32(dgvBooks.SelectedRows[0].Cells["iDDataGridViewTextBoxColumn"].Value);
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    string query = "UPDATE Books SET Title=@Title, Author=@Author, Genre=@Genre, YearPublished=@YearPublished WHERE ID=@iDDataGridViewTextBoxColumn";
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        // Add parameters
+                        cmd.Parameters.AddWithValue("@Title", TxtTitle.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Author", TxtAuthor.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Genre", TxtGenre.Text.Trim());
+                        cmd.Parameters.AddWithValue("@YearPublished", TxtYear.Text.Trim());
+                        cmd.Parameters.AddWithValue("@iDDataGridViewTextBoxColumn", id); // Set the ID parameter
+
+                        try
+                        {
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Record updated successfully!");
+                            RefreshDataGrid(); // Refresh DataGridView after update
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error updating record: " + ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to update.");
+            }
+        }
+        private void LoadData(int id)
+        {
+            ID = id;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Define the SQL query to retrieve data for a specific ID
+                    string selectQuery = "SELECT Title, Author, YearPublished, Genre FROM Books WHERE ID = @iDDataGridViewTextBoxColumn";
+                    SqlCommand command = new SqlCommand(selectQuery, connection);
+                    command.Parameters.AddWithValue("@iDDataGridViewTextBoxColumn", id);
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        // Populate form fields with data from the database
+                        TxtTitle.Text = reader["Title"].ToString();
+                        TxtAuthor.Text = reader["Author"].ToString();
+                        TxtYear.Text = reader["YearPublished"].ToString();
+                        TxtGenre.Text = reader["Genre"].ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Record not found.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading data: " + ex.Message);
+                }
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvBooks.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dgvBooks.SelectedRows[0];
+                int id = Convert.ToInt32(selectedRow.Cells["iDDataGridViewTextBoxColumn"].Value); // Assuming "ID" is the column name for your primary key
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    string deleteQuery = "DELETE FROM Books WHERE ID = @iDDataGridViewTextBoxColumn";
+                    using (SqlCommand cmd = new SqlCommand(deleteQuery, con))
+                    {
+                        cmd.Parameters.AddWithValue("@iDDataGridViewTextBoxColumn", id);
+
+                        try
+                        {
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Record deleted successfully!");
+                            RefreshDataGrid(); // Refresh DataGridView after deletion
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error deleting record: " + ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to delete.");
+            }
+
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            TxtTitle.Text = string.Empty;
+            TxtAuthor.Text = string.Empty;
+            TxtGenre.Text = string.Empty;
+            TxtYear.Text = string.Empty;
+
+            // Optionally, you can clear DataGridView selection
+            dgvBooks.ClearSelection();
+    }
+
+        private void dgvBooks_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
